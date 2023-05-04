65bca19dc6bda9d856d201eec34584e1f960d5e7
Mariana <mrc.marii.04@gmail.com>
2023-05-04 02:59:25 -0300

    transferencias

---
+++ src/main/java/com/mindhub/homebanking/Controller/TransactionController.java
@@ -0,0 +1,12 @@
+package com.mindhub.homebanking.Controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@RequestMapping("/api/clients/")
+public class TransferController {
+
+}
